0e077e15410a6faace6a35cb70177dbc2c264eb4
fengqingyang <fengqingyang@tsign.cn>
2022-06-27 14:54:34 +0800

    daily commit

--- public/js/douban.js
+++ public/js/douban.js
@@ -5,3 +5,5 @@ var _hmt = _hmt || [];
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3531608925757879"
+     crossorigin="anonymous"></script>
